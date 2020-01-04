b2a9a42e1dba4a0cde48e814e43123c6083a6973
Christoph Graf <christoph.graf.01@gmail.com>
2020-01-05 00:41:55 +0100

    fixed lose condition

--- Assets/FireCheck.cs
+++ Assets/FireCheck.cs
@@ -19,12 +19,21 @@ public class FireCheck : MonoBehaviour
     private void CheckForFireLine()
     {
         ContactFilter2D cf = new ContactFilter2D();
-        cf.NoFilter();
+        cf.useLayerMask = true;
+        cf.layerMask = fireLayerMask;
+        cf.useTriggers = true;
         List<RaycastHit2D> hitInfos = new List<RaycastHit2D>();
 
         Physics2D.Raycast(transform.position, Vector2.right, cf, hitInfos, 50);
 
-        if (hitInfos.Count == 8)
+        Debug.Log("fires: " + hitInfos.Count);
+        Debug.DrawRay(transform.position, Vector3.right, Color.red, 2f);
+        foreach (var fire in hitInfos)
+        {
+            Debug.Log(fire.transform.name);
+        }
+        
+        if (hitInfos.Count >= 8)
             StartCoroutine(GameManager.Instance.LoseGame());
     }
 }
